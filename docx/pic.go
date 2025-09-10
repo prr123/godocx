60489440e7dd0e642b2c252b679834e3bca204f5
Baoshuo <i@baoshuo.ren>
2025-09-10 14:37:40 +0800

    feat: add alt text support

--- docx/pic.go
+++ docx/pic.go
@@ -3,6 +3,7 @@ package docx
 import (
 	"github.com/gomutex/godocx/common/units"
 	"github.com/gomutex/godocx/dml"
+	"github.com/gomutex/godocx/dml/dmlpic"
 )
 
 type PicMeta struct {
@@ -38,4 +39,35 @@ func (rd *RootDoc) AddPicture(path string, width units.Inch, height units.Inch) 
 	rd.Document.Body.Children = append(rd.Document.Body.Children, bodyElem)
 
 	return p.AddPicture(path, width, height)
+}
+
+// SetAltText sets the alternative text (alt text) for the picture.
+//
+// Alt text is used to provide a textual description of the image for accessibility purposes.
+// It is important for users who rely on screen readers or have images disabled in their browsers.
+//
+// Example usage:
+//
+//	// Set alternative text for the picture
+//	picMeta.SetAltText("Gopher", "A cute gopher mascot")
+//
+// Parameters:
+//   - title: The title of the image, which serves as a brief description.
+//   - desc: The description of the image, providing more detailed information.
+func (pm *PicMeta) SetAltText(title, desc string) {
+	if pm.Inline == nil {
+		return
+	}
+
+	pm.Inline.DocProp.Name = title
+	pm.Inline.DocProp.Description = desc
+
+	if pm.Inline.Graphic.Data == nil {
+		pm.Inline.Graphic.Data = &dml.GraphicData{}
+	}
+	if pm.Inline.Graphic.Data.Pic == nil {
+		pm.Inline.Graphic.Data.Pic = &dmlpic.Pic{}
+	}
+	pm.Inline.Graphic.Data.Pic.NonVisualPicProp.CNvPr.Name = title
+	pm.Inline.Graphic.Data.Pic.NonVisualPicProp.CNvPr.Description = desc
 }
