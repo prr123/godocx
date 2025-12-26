a18f261c18dc1724f1d717375e62f60482a22b0f
Peter <azulpeter@azulcloud2.tail1b332.ts.net>
2025-12-26 14:32:50 +0100

    change ref from gomutex to prr123

---
+++ docx/document.go
@@ -0,0 +1,125 @@
+package docx
+
+import (
+
+	"encoding/xml"
+
+    "github.com/prr123/godocx/internal"
+    "github.com/prr123/godocx/wml/stypes"
+)
+
+var docAttrs = []xml.Attr{
+	{Name: xml.Name{Local: "xmlns:w"}, Value: "http://schemas.openxmlformats.org/wordprocessingml/2006/main"},
+	{Name: xml.Name{Local: "xmlns:o"}, Value: "urn:schemas-microsoft-com:office:office"},
+	{Name: xml.Name{Local: "xmlns:r"}, Value: "http://schemas.openxmlformats.org/officeDocument/2006/relationships"},
+	{Name: xml.Name{Local: "xmlns:v"}, Value: "urn:schemas-microsoft-com:vml"},
+	{Name: xml.Name{Local: "xmlns:w10"}, Value: "urn:schemas-microsoft-com:office:word"},
+	{Name: xml.Name{Local: "xmlns:wp"}, Value: "http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing"},
+	{Name: xml.Name{Local: "xmlns:wps"}, Value: "http://schemas.microsoft.com/office/word/2010/wordprocessingShape"},
+	{Name: xml.Name{Local: "xmlns:wpg"}, Value: "http://schemas.microsoft.com/office/word/2010/wordprocessingGroup"},
+	{Name: xml.Name{Local: "xmlns:mc"}, Value: "http://schemas.openxmlformats.org/markup-compatibility/2006"},
+	{Name: xml.Name{Local: "xmlns:wp14"}, Value: "http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing"},
+	{Name: xml.Name{Local: "xmlns:w14"}, Value: "http://schemas.microsoft.com/office/word/2010/wordml"},
+	{Name: xml.Name{Local: "xmlns:w15"}, Value: "http://schemas.microsoft.com/office/word/2012/wordml"},
+	{Name: xml.Name{Local: "mc:Ignorable"}, Value: "w14 wp14 w15"},
+}
+
+// This element specifies the contents of a main document part in a WordprocessingML document.
+type Document struct {
+	// Reference to the RootDoc
+	Root *RootDoc
+
+	// Elements
+	Background *Background
+	Body       *Body
+
+	// Non elements - helper fields
+	DocRels      Relationships // DocRels represents relationships specific to the document.
+	RID          int
+	relativePath string
+}
+
+// IncRelationID increments the relation ID of the document and returns the new ID.
+// This method is used to generate unique IDs for relationships within the document.
+func (doc *Document) IncRelationID() int {
+	doc.RID += 1
+	return doc.RID
+}
+
+// MarshalXML implements the xml.Marshaler interface for the Document type.
+func (doc Document) MarshalXML(e *xml.Encoder, start xml.StartElement) (err error) {
+	start.Name.Local = "w:document"
+
+	start.Attr = append(start.Attr, docAttrs...)
+
+	err = e.EncodeToken(start)
+	if err != nil {
+		return err
+	}
+
+	if doc.Background != nil {
+		if err = doc.Background.MarshalXML(e, xml.StartElement{}); err != nil {
+			return err
+		}
+	}
+
+	if doc.Body != nil {
+		bodyElement := xml.StartElement{Name: xml.Name{Local: "w:body"}}
+		if err = e.EncodeElement(doc.Body, bodyElement); err != nil {
+			return err
+		}
+	}
+
+	return e.EncodeToken(xml.EndElement{Name: start.Name})
+}
+
+func (d *Document) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) (err error) {
+
+	for {
+		currentToken, err := decoder.Token()
+		if err != nil {
+			return err
+		}
+
+		switch elem := currentToken.(type) {
+		case xml.StartElement:
+			switch elem.Name.Local {
+			case "body":
+				body := NewBody(d.Root)
+				if err := decoder.DecodeElement(body, &elem); err != nil {
+					return err
+				}
+				d.Body = body
+			case "background":
+				bg := NewBackground()
+				if err := decoder.DecodeElement(bg, &elem); err != nil {
+					return err
+				}
+				d.Background = bg
+			default:
+				if err = decoder.Skip(); err != nil {
+					return err
+				}
+			}
+		case xml.EndElement:
+			return nil
+		}
+	}
+
+}
+
+// AddPageBreak adds a page break to the document by inserting a paragraph containing only a page break.
+//
+// Returns:
+//   - *Paragraph: A pointer to the newly created Paragraph object containing the page break.
+//
+// Example:
+//
+//	document := godocx.NewDocument()
+//	para := document.AddPageBreak()
+func (rd *RootDoc) AddPageBreak() *Paragraph {
+	p := rd.AddEmptyParagraph()
+	p.AddRun().AddBreak(internal.ToPtr(stypes.BreakTypePage))
+
+	return p
+}
---
+++ docx/writer.go
@@ -0,0 +1,150 @@
+package docx
+
+import (
+
+	"archive/zip"
+	"errors"
+	"io"
+	"os"
+	"path/filepath"
+	"sort"
+	"time"
+
+    "github.com/prr123/godocx/common/constants"
+)
+
+// Close method is used to close the RootDoc. Currently, it does not perform any specific actions.
+func (rd *RootDoc) Close() error {
+	var err error
+
+	return err
+}
+
+// Write method writes the RootDoc to an io.Writer.
+func (rd *RootDoc) Write(w io.Writer) error {
+	_, err := rd.WriteTo(w)
+	return err
+}
+
+// WriteTo implements io.WriterTo to write the RootDoc to an io.Writer.
+func (rd *RootDoc) WriteTo(w io.Writer) (int64, error) {
+
+	if err := rd.writeDirectToWriter(w); err != nil {
+		return 0, err
+	}
+	return 0, nil
+}
+
+// writeDirectToWriter writes the RootDoc directly to an io.Writer using a zip.Writer.
+func (rd *RootDoc) writeDirectToWriter(w io.Writer) error {
+	zw := zip.NewWriter(w)
+	if err := rd.writeToZip(zw); err != nil {
+		_ = zw.Close()
+		return err
+	}
+	return zw.Close()
+}
+
+// writeToZip provides a function to write to zip.Writer
+func (rd *RootDoc) writeToZip(zw *zip.Writer) error {
+
+	var (
+		err   error
+		files []string
+	)
+
+	// Build a local deterministic snapshot rather than mutating rd.FileMap while writing
+	snapshot := make(map[string][]byte)
+
+	ct, err := marshal(rd.ContentType)
+	if err != nil {
+		return err
+	}
+	snapshot[constants.ConentTypeFileIdx] = []byte(ct)
+
+	docRelContent, err := marshal(rd.Document.DocRels)
+	if err != nil {
+		return err
+	}
+	snapshot[rd.Document.DocRels.RelativePath] = docRelContent
+
+	rootRelContent, err := marshal(rd.RootRels)
+	if err != nil {
+		return err
+	}
+	snapshot[rd.RootRels.RelativePath] = rootRelContent
+
+	docContent, err := marshal(rd.Document)
+	if err != nil {
+		return err
+	}
+	snapshot[rd.Document.relativePath] = docContent
+
+	docStyleBytes, err := marshal(rd.DocStyles)
+	if err != nil {
+		return err
+	}
+	snapshot[rd.DocStyles.RelativePath] = docStyleBytes
+
+	// Persist numbering instances into numbering.xml if any
+	if rd.Numbering != nil {
+		// Apply numbering into a temporary buffer based on either existing or minimal content
+		// by temporarily swapping root to a shallow copy with only the snapshot layer.
+		// Minimal change: read existing numbering.xml if present from rd.FileMap, else let manager create it.
+		if err := rd.Numbering.applyToFileMap(); err != nil {
+			return err
+		}
+	}
+
+	// Collect files from original map
+	rd.FileMap.Range(func(path, content any) bool {
+		p := path.(string)
+		if _, exists := snapshot[p]; !exists {
+			snapshot[p] = content.([]byte)
+		}
+		return true
+	})
+
+	// Now gather list of paths from snapshot
+	for p := range snapshot {
+		files = append(files, p)
+	}
+
+	sort.Strings(files)
+	for _, path := range files {
+		// Use a deterministic timestamp for reproducible archives
+		hdr := &zip.FileHeader{
+			Name:     path,
+			Method:   zip.Deflate,
+			Modified: time.Unix(0, 0).UTC(),
+		}
+		var fi io.Writer
+		if fi, err = zw.CreateHeader(hdr); err != nil {
+			break
+		}
+		_, err = fi.Write(snapshot[path])
+	}
+
+	return err
+}
+
+// Save method saves the RootDoc to the specified file path.
+func (rd *RootDoc) Save() error {
+	return rd.SaveTo(rd.Path)
+}
+
+// SaveTo method saves the RootDoc to the specified file path.
+func (rd *RootDoc) SaveTo(fileName string) error {
+	if fileName == "" {
+		return errors.New("Destination file path is empty")
+	}
+
+	file, err := os.OpenFile(filepath.Clean(fileName), os.O_WRONLY|os.O_TRUNC|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return rd.Write(file)
+
+}
