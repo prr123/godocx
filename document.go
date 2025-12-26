aa0d45508001621ce160b3fd8e039397a88b24a0
Peter <azulpeter@azulcloud2.tail1b332.ts.net>
2025-12-26 14:26:29 +0100

    change ref from gomutex to prr123

---
+++ document.go
@@ -0,0 +1,28 @@
+package godocx
+
+import (
+
+	_ "embed"
+	"os"
+	"path/filepath"
+
+    "github.com/prr123/godocx/docx"
+    "github.com/prr123/godocx/packager"
+)
+
+//go:embed templates/default.docx
+var defaultDocx []byte
+
+// NewDocument creates a new document from the default template.
+func NewDocument() (*docx.RootDoc, error) {
+	return packager.Unpack(&defaultDocx)
+}
+
+// OpenDocument opens a document from the given file name.
+func OpenDocument(fileName string) (*docx.RootDoc, error) {
+	docxContent, err := os.ReadFile(filepath.Clean(fileName))
+	if err != nil {
+		return nil, err
+	}
+	return packager.Unpack(&docxContent)
+}
